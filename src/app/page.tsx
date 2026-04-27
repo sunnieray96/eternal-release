08631e1614b5ddf44e4892ddd41002c7a2b4613f
sunnieray96 <sunnieray47@gmail.com>
2026-04-27 03:06:40 -0700

    MASTER REDESIGN: Story-Driven Sanctuary. Atomic push to fix all Vercel errors. Standardizing on Next 15 + Tailwind 4.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="bg-[#FCFAF7]">
+    <main>
       <Navbar />
       <Hero />
       <IntroductionTeaser />
